80003bdc2667d3faf39e803ce887d474904985a5
BucurEva87 <bucur.eva87@gmail.com>
2023-03-04 18:34:23 +0200

    Modify project as requested by reviewer

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   # get 'users/index'
   # get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users',                     to: 'users#index'
-  get '/users/:id',                 to: 'users#show'
-  get '/users/:user_id/posts',      to: 'posts#index'
-  get '/users/:user_id/posts/:id',  to: 'posts#show'
+  resources :users do
+    resources :posts
+  end
 
   # Defines the root path route ("/")
   root "users#index"
