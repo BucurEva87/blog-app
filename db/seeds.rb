b042437ed030836b7cb8abf46554f515d032d880
BucurEva87 <bucur.eva87@gmail.com>
2023-03-06 13:36:00 +0200

    Add seeds for users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create some users
+User.create(name: "Tom", photo: "https://randomuser.me/api/portraits/men/60.jpg", bio: "Teacher from Mexico.", posts_counter: 0)
+User.create(name: "Lilly", photo: "https://randomuser.me/api/portraits/women/56.jpg", bio: "Teacher from Poland.", posts_counter: 0)
