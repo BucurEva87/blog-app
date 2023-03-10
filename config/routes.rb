d3ea85ffa85dd7f26bae6480b55f6d5694560102
V-Blaze <valentine6586@gmail.com>
2023-03-10 14:41:07 +0100

    Add API routes and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,16 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index show create] do
+        resources :posts, only: %i[index show create] do
+          resources :comments, only: %i[index show create]
+        end
+      end
+    end
+  end
+
   # Defines the root path route ("/")
   root "users#index"
 end
